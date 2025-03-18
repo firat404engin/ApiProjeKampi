0f5d5137bdc11bf8d09a91681d443c8fd8fc8ed4
firatengin404 <62977294+firat404engin@users.noreply.github.com>
2025-03-18 23:53:16 +0300

    Validator

---
+++ ApiProjeKampi.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using ApiProjeKampi.WebApi.Entities;
+using FluentValidation;
+
+namespace ApiProjeKampi.WebApi.ValidationRules
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+
+        public ProductValidator()
+        {
+            RuleFor(x=>x.ProductName).NotEmpty().WithMessage("Lütfen Ürün Adını Boş Geçmeyin");
+            RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("2 karakter girişi yapın");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("50 karakter girişi yapınız");
+
+            RuleFor(x=>x.Price).NotEmpty().WithMessage("Ürün fiyatı boş geçilemez").LessThan(0).WithMessage("Ürün fiyatı negatif olamaz").GreaterThan(1000).WithMessage("Ürün fiyatı bu kadar yüksek olamaz , değeri kontrol edin");
+
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("Ürün açıklaması boş geçilemez");
+        }
+
+    }
+}
